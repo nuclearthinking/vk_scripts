6a86d479579f9298671a523c5241d7e31df71a05
Vladislav Radchenko <v.radchenko@cian.ru>
2019-01-29 14:13:47 +0300

    get things done!

--- script.py
+++ script.py
@@ -1,10 +1,20 @@
 import datetime
+import time
 from enum import Enum
 
 import vk
+from vk.exceptions import VkAPIError
+
 
 today = datetime.date.today()
 ADULT_BIRTH_DATE = datetime.date(today.year - 18, today.month, today.day)
+
+BAN_COMMENTARY = 'Бан по ограничению 18+ (предварительно). Для разбана/подтверждения возраста написать любому из ' \
+                 'администраторов группы.'
+
+access_token = "TOKEN"
+session = vk.Session(access_token=access_token)
+vk_api = vk.API(session, v='5.92')
 
 
 class Status(Enum):
@@ -16,15 +26,55 @@ class Status(Enum):
 class Subscriber:
     id: int
     status: Status
+    years: int
 
-    def __init__(self, id: int, status: Status) -> None:
+    def __init__(self, id: int, status: Status, years: int = None) -> None:
         self.id = id
         self.status = status
+        self.years = years
+
+    def ban(self):
+        try:
+            vk_api.groups.ban(
+                group_id=121768940,
+                owner_id=self.id,
+                comment=BAN_COMMENTARY,
+                comment_visible=1
+            )
+        except VkAPIError as e:
+            if 'Captcha needed' not in e.message:
+                raise e
+            data = e.error_data
+            captcha_sid = data.get('captcha_sid')
+            captcha_img = data.get('captcha_img')
+
+            print(f'CAPTCHA LINK {captcha_img}')
+            captcha_key = input('Input text from captcha: ')
+
+            try:
+                vk_api.groups.ban(
+                    group_id=121768940,
+                    owner_id=self.id,
+                    comment=BAN_COMMENTARY,
+                    comment_visible=1,
+                    captcha_sid=captcha_sid,
+                    captcha_key=captcha_key
+                )
+            except:
+                pass
 
 
-def chunks(l, n):
-    n = max(1, n)
-    return (l[i:i + n] for i in range(0, len(l), n))
+def fetch_items(func, **kwargs):
+    result = list()
+    count, counter = 1, 0
+    while counter < count:
+        response = func(offset=counter, **kwargs)
+        count = response.get('count')
+        items = response.get('items')
+        result.extend(items)
+        counter += len(items)
+        time.sleep(0.5)
+    return result
 
 
 def has_birth_year(bdate: str):
@@ -47,49 +97,50 @@ def str_to_date(birth_date: str) -> datetime.date:
     )
 
 
-def ban_user(user_id: int):
-    vk_api.groups.ban(
-        group_id=121768940,
-        owner_id=user_id,
-        comment='18+',
-        comment_visible=1
-    )
+def main():
+    banned_users = fetch_items(vk_api.groups.getBanned, group_id=121768940, count=200)
+    subscribers = fetch_items(vk_api.groups.getMembers, group_id='box_review', fields='bdate')
+
+    banned_ids = [user['profile']['id'] for user in banned_users if user.get('profile')]
+    subscribers_data = list()
+
+    for user in subscribers:
+        id = user.get('id')
+        bdate = user.get('bdate')
+
+        if id in banned_ids:
+            print(f'User {id} already banned')
+            continue
+
+        if not bdate:
+            subscribers_data.append(Subscriber(id=id, status=Status.unknown))
+            continue
+
+        if not has_birth_year(bdate):
+            subscribers_data.append(Subscriber(id=id, status=Status.unknown))
+            continue
+
+        birth_date = str_to_date(bdate)
+        life_span = today - birth_date
+        years = life_span.days // 365
+
+        if years >= 18:
+            subscribers_data.append(Subscriber(id=id, status=Status.is_adult, years=years))
+            continue
+
+        subscribers_data.append(Subscriber(id=id, status=Status.not_adult, years=years))
+
+    children = [sub for sub in subscribers_data if sub.status in [Status.not_adult]]
+
+    print(f'{len(children)} users wil be banned')
+    for child in children:
+        print(f'Ban user {child.id}')
+        child.ban()
+        print(f'User {child.id} successfully banned')
+        time.sleep(0.3)
+
+    print('Done')
 
 
-subscribers_data = list()
-
-access_token = "TOKEN"
-session = vk.Session(access_token=access_token)
-vk_api = vk.API(session, v='5.92')
-
-response = vk_api.groups.getMembers(group_id='box_review', fields='bdate')
-count = response.get('count')
-subscribers = response.get('items')
-offset = len(subscribers)
-
-for user in subscribers:
-    id = user.get('id')
-    bdate = user.get('bdate')
-
-    if not bdate:
-        subscribers_data.append(Subscriber(id=id, status=Status.unknown))
-        continue
-
-    if not has_birth_year(bdate):
-        subscribers_data.append(Subscriber(id=id, status=Status.unknown))
-        continue
-
-    if is_adult(str_to_date(bdate)):
-        subscribers_data.append(Subscriber(id=id, status=Status.is_adult))
-        continue
-
-    subscribers_data.append(Subscriber(id=id, status=Status.not_adult))
-
-children = [sub for sub in subscribers_data if sub.status in [Status.not_adult, Status.unknown]]
-
-for child in children:
-    print(f'Ban user {child.id}')
-    ban_user(child.id)
-
-
-print('break point')
+if __name__ == '__main__':
+    main()
